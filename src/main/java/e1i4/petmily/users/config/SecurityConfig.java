639dad56b310ad1b9d93fdebdb519e9fd7920b1f
TaeHoon <taehoondev030@gamil.com>
2025-08-25 14:59:38 +0900

    OAuth2UserService 응답 받기

--- src/main/java/e1i4/petmily/users/config/SecurityConfig.java
+++ src/main/java/e1i4/petmily/users/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package e1i4.petmily.users.config;
 
+import e1i4.petmily.users.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,7 +32,9 @@ public class SecurityConfig {
         http.httpBasic((auth) -> auth.disable());
 
         // oauth2
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login((oauth2) -> oauth2
+                .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                        .userService(customOAuth2UserService)));
 
         // 경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth
---
+++ src/main/java/e1i4/petmily/users/service/CustomOAuth2UserService.java
@@ -0,0 +1,31 @@
+package e1i4.petmily.users.service;
+
+import e1i4.petmily.users.dto.GoogleResponse;
+import e1i4.petmily.users.dto.OAuth2Response;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("oAuth2User = " + oAuth2User); // 로그 확인용
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if(registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        } else {
+
+            return null;
+        }
+
+    }
+}
